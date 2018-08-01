d4601a4b137c4a03d676af418b18244e95e46dd0
Nzingani <isabelnzingani@outlook.com>
2018-08-01 11:17:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P7ex1</title>
+  </head>
+  <body>
+  <form action='user.php' method='Post'>
+    <input type='text'name='lastname'placeholder="nom">
+    <input type='text'name='firstname'placeholder="prénom">
+    <input type='submit'value='valider'>
+  </form>
+  </body>
+</html>
---
+++ user.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!DOCTYPE html>
+    <html lang="fr" dir="ltr">
+      <head>
+        <meta charset="utf-8" />
+        <title>user.php</title>
+      </head>
+      <body>
+        <p>
+        <?php
+        // if (isset($_POST['firstName']) AND isset($_POST['lastName']))
+     // {
+     //   echo 'Bonjour, ' . ($_POST['firstName']) . ' ' . ($_POST['lastName']);
+     // }
+       ?>
+     </p>
+      </body>
+    </html>
+
+  </body>
+</html>
